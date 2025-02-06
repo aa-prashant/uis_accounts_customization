0dffa6bea930b31729ce8ecfdfbc18f4faafe5a5
IamSaiyyamChhetri <saiyyam@8848digital.com>
2025-02-06 23:41:56 +0530

    fix: Add validation for Budget cust for item

--- uis/uis/custom/customization_script/purchase_invoice.py
+++ uis/uis/custom/customization_script/purchase_invoice.py
@@ -3,6 +3,9 @@ from uis.uis.api.utils import get_allocated_amount
 
 def validate(doc, method):
     for item in doc.items:
-        allocated_left_budget = get_allocated_amount(doc, item)
-        if allocated_left_budget < item.amount:
-            frappe.throw(f"Amount cannot exceed then allocate budget.<br> For Item : {frappe.bold(item.item_code)}  in row no. {item.idx}.")
+        is_fixed_asset = frappe.db.get_value("Item",item.item_code, "is_fixed_asset" )
+        if is_fixed_asset:
+            allocated_left_budget = get_allocated_amount(doc, item)
+            if allocated_left_budget < item.amount:
+                frappe.throw(f"Amount cannot exceed then allocate budget.<br> For Item : {frappe.bold(item.item_code)}  in row no. {item.idx}.")
+        else: return True
