41df1e88b5063f2b5221a577306e65ecb67cbdc6
Actinoids-IO <hello@actinoids-io.in>
2025-02-23 18:27:22 +0530

    feat: UIS Allocated Budget dt

---
+++ uis/uis/doctype/uis___allocate_budget/uis___allocate_budget.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mohamed Elyamany and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("UIS - Allocate Budget", {
+// 	refresh(frm) {
+
+// 	},
+// });
