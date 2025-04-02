7629c12ece818fa86f7684e9070c775e7f234313
Actinoids-IO <hello@actinoids-io.in>
2025-04-02 18:25:36 +0530

    feat: Country State and City format - Job Order - Lead - Technical Inquiry

--- uis/hooks.py
+++ uis/hooks.py
@@ -40,7 +40,8 @@ app_include_js = "uis.bundle.js"
 doctype_js = {
     "Purchase Invoice" : "uis/custom/customization_dt/purchase_invoice.js",
     "Item Group":"uis/custom/customization_dt/item_group.js",
-    "Address":"uis/custom/customization_dt/address.js"
+    "Address":"uis/custom/customization_dt/address.js",
+    "Lead":"uis/custom/customization_dt/lead.js"
 
     
     }
