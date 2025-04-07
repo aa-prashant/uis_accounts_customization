a05d78cda7a7143e5b3bbac75730f015c80f87b8
Actinoids-IO <hello@actinoids-io.in>
2025-04-07 11:55:27 +0530

    fix: Conosolidate Report - Fix currency exchange

--- uis/uis/custom/customization_script/sales_order.py
+++ uis/uis/custom/customization_script/sales_order.py
@@ -3,4 +3,4 @@ from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
 
 class OverrideSalesOrder(SalesOrder):
     def validate_update_after_submit(self):
-        print("Hello World")
+        pass
