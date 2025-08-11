cbacc5ac2f9da77aa01a28671fc7d990c15095a6
Prashant Agrawal <aaprashant706@gmail.com>
2025-08-11 10:24:09 +0000

    removed stale doctypes

--- uis_accounts_customization/other_customization/doctype/journey_request/journey_request.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Mohamed Elyamany and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class JourneyRequest(Document):
-	pass
